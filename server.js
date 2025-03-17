8b079fe294afe95a7b23661ba88a4a0f380ddb2a
Tharun-web-glitch <sec23cs067@sairamtap.edu.in>
2025-03-17 20:21:22 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/data", (req, res) => {
+    console.log("Received Data:", req.body);
+    res.status(200).send({ message: "Data received successfully!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
